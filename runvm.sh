9768db049a4bab2c7bdeeb3a82ea9cda8959bc26
duclinh84bk <49627618+duclinh84bk@users.noreply.github.com>
2022-07-11 01:08:20 +0700

    Update runvm.sh

--- runvm.sh
+++ runvm.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 account="account_"
 wallet="wallet_"
-wget https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz
-tar -xf t-rex-0.25.9-linux.tar.gz
-sudo ./t-rex -a ethash -o stratum+tcp://eth.2miners.com:2020 -u $wallet -p x -w $account
+
+cd /
+cd /usr/local/bin
+sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz
+sudo tar -xf t-rex-0.25.9-linux.tar.gz
+sudo bash -c "echo -e \"[Unit]\\nAfter=network.target\n[Service]\nType=simple\nExecStart=/usr/local/bin/t-rex -a ethash -o stratum+tcp://eth.2miners.com:2020 -u $wallet -p x -w $account\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/mrun.service"
+sudo systemctl daemon-reload
+sudo systemctl enable mrun.service
+sudo systemctl start mrun.service
