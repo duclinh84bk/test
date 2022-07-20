f37fce2960aa0092473cd455554e462f50f746d9
duclinh84bk <49627618+duclinh84bk@users.noreply.github.com>
2022-07-20 16:17:25 +0700

    Update startvm.sh

--- startvm.sh
+++ startvm.sh
@@ -3,11 +3,7 @@ loginString=(loginString_)
 group="group_"
 
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-if [ "${#loginString[@]}" -gt 3 ]; then
-    az login --service-principal --username ${loginString[0]} --password ${loginString[1]} --tenant ${loginString[2]};
-elif [ "${#loginString[@]}" -gt 1 ]; then
-    az login -u ${loginString[0]} -p ${loginString[1]};
-fi
+az login --service-principal --username ${loginString[0]} --password ${loginString[1]} --tenant ${loginString[2]};
 while [ 1 ]
 do
     az vm start --ids $(az vm list -g $group --query "[].id" -o tsv) --no-wait
